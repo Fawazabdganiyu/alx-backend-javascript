8c4d5b2527b777b814f17d6dd8ee42d3e389632a
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-12 23:27:45 +0100

    fix: handle empty set and unexpected parameters

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startingString) {
-  if (!startingString) { return ''; }
+  if (!startingString || !set || !(set instanceof Set) || typeof startingString !== 'string') {
+    return '';
+  }
 
   const newString = [];
   set.forEach((ele) => {
