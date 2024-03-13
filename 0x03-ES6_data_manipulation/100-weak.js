9565170131f676a5c1c5baeda988389bb509ad3c
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-13 05:12:19 +0100

    Create Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+const weakMap = new WeakMap();
+const queryAPI = (endpoint) => {
+  const count = (weakMap.get(endpoint) || 0) + 1;
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, count);
+};
+
+export { weakMap, queryAPI };
