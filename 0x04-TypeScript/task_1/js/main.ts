174c00ecfc628e2ed1adfd83a4479b1be989a21b
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-20 22:37:18 +0100

    Define a class with interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,10 +7,6 @@ export interface Teacher {
   [key: string]: any;
 }
 
-interface Director extends Teacher {
-  numberOfReport: number;
-}
-
 // Example
 const teacher1: Teacher = {
   firstName: 'John',
@@ -21,3 +17,47 @@ const teacher1: Teacher = {
 };
 
 console.log(teacher1)
+
+interface Director extends Teacher {
+  numberOfReport: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+// Example
+const teacher2 = printTeacher("John", "Doe")
+console.log(teacher2)
+
+
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface{
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working'
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
