6938fb4dd3141b013d31bf590758d297dff4a0e0
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-30 22:34:33 +0100

    Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const { createServer } = require('node:http');
+
+const port = 12345;
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port);
