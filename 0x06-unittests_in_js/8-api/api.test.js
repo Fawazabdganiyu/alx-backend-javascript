5b2afe982482c8f12f3e7b48a1fd26d9d7e7e768
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-05-02 23:52:40 +0100

    Regex integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,10 +2,11 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('API index page tests', () => {
-  it('should respond with correct status code and welcome message', () => {
-    request('http://localhost:7865', (error, response, body) => {
+  it('should respond with correct status code and welcome message', (done) => {
+    request('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
 });
